cbd98d92d3b8a89ab84bfdb49d25f7b2025fb011
Samuel Ménard <samuel.menard.fr@gmail.com>
2014-10-30 14:23:21 +0100

    Update file

--- src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
@@ -23,6 +23,8 @@ import javax.persistence.MappedSuperclass;
  * Simple JavaBean domain object adds a name property to <code>BaseEntity</code>. Used as a base class for objects
  * needing these properties.
  *
+ *
+ *
  * @author Ken Krebs
  * @author Juergen Hoeller
  */
